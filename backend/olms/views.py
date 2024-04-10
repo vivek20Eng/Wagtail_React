e59867526d880e8444992c6cc767357f20a0b899
Vivek s <viveks@alokin.in>
2024-04-10 15:06:42 +0530

    Basic auth integration

--- backend/olms/views.py
+++ backend/olms/views.py
@@ -14,31 +14,3 @@ from .models import *
 # def hello_world(request):
 #     return Response({'message': 'Hello, world!'})
 
-@api_view(['GET'])
-# Define a view function for the login page
-def login_page(request):
-    # Check if the HTTP request method is POST (form submission)
-    if request.method == "GET":
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-         
-        # Check if a user with the provided username exists
-        if not User.objects.filter(username=username).exists():
-            # Display an error message if the username does not exist
-            messages.error(request, 'Invalid Username')
-            return Response({'message': username})
-         
-        # Authenticate the user with the provided username and password
-        user = authenticate(username=username, password=password)
-         
-        if user is None:
-            # Display an error message if authentication fails (invalid password)
-            messages.error(request, "Invalid Password")
-            return Response({'message': 'Hello, world!'})
-        else:
-            # Log in the user and redirect to the home page upon successful login
-            login(request, user)
-            return Response({'message': 'Hello, world!'})
-     
-    # Render the login page template (GET request)
-    return render(request, 'login.html')
